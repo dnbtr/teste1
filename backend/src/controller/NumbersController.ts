74ff5d766ba38818f5cce0cce67dc85343d2be96
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-25 17:20:10 -0300

    feat(backend): NumbersController finished

--- backend/src/controller/NumbersController.ts
+++ backend/src/controller/NumbersController.ts
@@ -6,10 +6,15 @@ export default class NumbersController {
     const { number1, number2, number3 } = req.body;
     const { signed } = req.query;
 
-    // Buffer.alloc(n), onde N é a quantidade de bytes
+    // Buffer.alloc(N), where N is the ammount of bytes
     let num1 = Buffer.alloc(4);
     let num2 = Buffer.alloc(2);
     let num3 = Buffer.alloc(1);
+
+    // Response variables
+    let buffer;
+    let bufferString;
+    let bufferNumeric;
 
     try {
       if (signed == 'true') {
@@ -21,10 +26,31 @@ export default class NumbersController {
         num2.writeUInt16BE(number2, 0);
         num3.writeUInt8(number3, 0);
       }
+
+      // Creating the final buffer (with 7 bytes)
+      buffer = Buffer.concat([
+        num1,
+        num2,
+        num3,
+      ]);
+
+      bufferString = buffer.toString('hex');
+      bufferNumeric = parseInt(bufferString, 16);
+
+      console.log(bufferNumeric)
+
     } catch (error) {
-      return res.status(400).send({ error: error, message: error.message });
+      return res.status(400).send({ status: 'error', error: error, message: error.message });
     }
 
-    return res.send('Hello World!');
+    return res.status(200).json({
+      status: 'ok',
+      buffer: {
+        length: buffer.length,
+        string: bufferString,
+        numeric: bufferNumeric,
+        raw: buffer
+      }
+    });
   }
 }
